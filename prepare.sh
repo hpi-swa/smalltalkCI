1db94d9e78b99031fd76600929464214694b94ef
Fabio Niephaus <code@fniephaus.com>
2015-07-10 16:25:46 +0200

    Make argument optional

--- prepare.sh
+++ prepare.sh
@@ -2,10 +2,8 @@
 
 set -e
 
-if [ -z "$1" ]
-  then
-    echo "No image name provided"
-fi
+IMAGE=$1
+[ -z "$IMAGE" ] && IMAGE=TrunkImage
 
 BASE_PATH="$(pwd)"
 TMP_PATH="$BASE_PATH/tmp"
@@ -13,7 +11,7 @@ BUILD_PATH="$BASE_PATH/build"
 IMAGE_PATH="$BASE_PATH/image"
 SCRIPTS_PATH="$BASE_PATH/scripts"
 VM_PATH="$BASE_PATH/vm"
-IMAGE_TAR="$1.tar.gz"
+IMAGE_TAR="$IMAGE.tar.gz"
 
 echo "Preparing folders..."
 mkdir "$TMP_PATH" "$BUILD_PATH"
@@ -23,8 +21,9 @@ cp -r "$IMAGE_PATH/" "$TMP_PATH/"
 cp -r "$SCRIPTS_PATH" "$TMP_PATH/scripts"
 
 echo "Preparing image for CI..."
-"$VM_PATH/Cog.app/Contents/MacOS/Squeak" "$TMP_PATH/$1" "$TMP_PATH/scripts/prepare.st"
+"$VM_PATH/Cog.app/Contents/MacOS/Squeak" "$TMP_PATH/$IMAGE" "$TMP_PATH/scripts/prepare.st"
 
+echo ""
 echo "Exporting image..."
 mv "$TMP_PATH/"{TravisCI.image,TravisCI.changes,*.sources} "$BUILD_PATH"
 cd "$BUILD_PATH"
