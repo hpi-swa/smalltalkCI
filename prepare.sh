4051a5afba8f3a5c5b5931a2d8cc715fddcef8bd
Fabio Niephaus <code@fniephaus.com>
2015-07-10 14:07:56 +0200

    Simplify and improve preparing process

--- prepare.sh
+++ prepare.sh
@@ -2,13 +2,18 @@
 
 set -e
 
+if [ -z "$1" ]
+  then
+    echo "No image name provided"
+fi
+
 BASE_PATH="$(pwd)"
 TMP_PATH="$BASE_PATH/tmp"
 BUILD_PATH="$BASE_PATH/build"
 IMAGE_PATH="$BASE_PATH/image"
 SCRIPTS_PATH="$BASE_PATH/scripts"
 VM_PATH="$BASE_PATH/vm"
-IMAGE_TAR="image.tar.gz"
+IMAGE_TAR="$1.tar.gz"
 
 echo "Preparing folders..."
 mkdir "$TMP_PATH" "$BUILD_PATH"
@@ -18,7 +23,7 @@ cp -r "$IMAGE_PATH/" "$TMP_PATH/"
 cp -r "$SCRIPTS_PATH" "$TMP_PATH/scripts"
 
 echo "Preparing image for CI..."
-"$VM_PATH/Cog.app/Contents/MacOS/Squeak" "$TMP_PATH/TrunkImage.image" "$TMP_PATH/scripts/prepare.st"
+"$VM_PATH/Cog.app/Contents/MacOS/Squeak" "$TMP_PATH/$1" "$TMP_PATH/scripts/prepare.st"
 
 echo "Exporting image..."
 mv "$TMP_PATH/"{TravisCI.image,TravisCI.changes,*.sources} "$BUILD_PATH"
