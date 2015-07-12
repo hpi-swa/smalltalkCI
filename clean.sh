4b8810fffb75690eb9164121eed6fcc24976924a
Fabio Niephaus <code@fniephaus.com>
2015-07-12 19:33:38 +0200

    Add simple clean script

---
+++ clean.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+function print_info {
+    printf "\e[0;34m$1\e[0m\n"
+}
+
+BASE_PATH="$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)"
+CACHE_PATH="$BASE_PATH/cache"
+BUILD_BASE="$BASE_PATH/builds"
+
+print_info "Cleaning up..."
+rm -rf "$CACHE_PATH" "$BUILD_BASE"
+
+print_info "Done!"
