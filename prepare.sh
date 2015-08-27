8eb895d76c68b412cc28e60be7330aa3409cc436
Fabio Niephaus <code@fniephaus.com>
2015-08-27 17:20:01 +0100

    Minor changes

--- prepare.sh
+++ prepare.sh
@@ -43,10 +43,10 @@ SCRIPTS_PATH="$BASE_PATH/scripts"
 SPUR_IMAGE=false
 case "$SMALLTALK" in
     "SqueakTrunk")
-        IMAGE_URL="http://build.squeak.org/job/Trunk/default/lastSuccessfulBuild/artifact/target/"
+        IMAGE_URL="http://build.squeak.org/job/Trunk/default/lastSuccessfulBuild/artifact/target"
         IMAGE_ARCHIVE="TrunkImage.zip"
         IMAGE_FILE="SpurTrunkImage.image"
-        SOURCES_URL="http://ftp.squeak.org/5.0/"
+        SOURCES_URL="http://ftp.squeak.org/5.0"
         SOURCES_ARCHIVE="SqueakV50.sources.zip"
         SOURCES_FILE="SqueakV50.sources"
         SPUR_IMAGE=true
@@ -54,27 +54,27 @@ case "$SMALLTALK" in
         DISABLE_UPDATE="true"
         ;;
     "Squeak5.0")
-        IMAGE_URL="http://ftp.squeak.org/5.0/"
+        IMAGE_URL="http://ftp.squeak.org/5.0"
         IMAGE_ARCHIVE="Squeak5.0-15113.zip"
         IMAGE_FILE="Squeak5.0-15113.image"
-        SOURCES_URL="http://ftp.squeak.org/5.0/"
+        SOURCES_URL="http://ftp.squeak.org/5.0"
         SOURCES_ARCHIVE="SqueakV50.sources.zip"
         SOURCES_FILE="SqueakV50.sources"
         SPUR_IMAGE=true
         ;;
     "Squeak4.6")
-        IMAGE_URL="http://ftp.squeak.org/4.6/"
+        IMAGE_URL="http://ftp.squeak.org/4.6"
         IMAGE_ARCHIVE="Squeak4.6-15102.zip"
         IMAGE_FILE="Squeak4.6-15102.image"
-        SOURCES_URL="http://ftp.squeak.org/4.6/"
+        SOURCES_URL="http://ftp.squeak.org/4.6"
         SOURCES_ARCHIVE="SqueakV46.sources.zip"
         SOURCES_FILE="SqueakV46.sources"
         ;;
     "Squeak4.5")
-        IMAGE_URL="http://ftp.squeak.org/4.5/"
+        IMAGE_URL="http://ftp.squeak.org/4.5"
         IMAGE_ARCHIVE="Squeak4.5-13680.zip"
         IMAGE_FILE="Squeak4.5-13680.image"
-        SOURCES_URL="http://ftp.squeak.org/4.5/"
+        SOURCES_URL="http://ftp.squeak.org/4.5"
         SOURCES_ARCHIVE="SqueakV41.sources.zip"
         SOURCES_FILE="SqueakV41.sources"
         ;;
@@ -142,11 +142,11 @@ if [ ! -f "$COG_VM_PATH" ]; then
 fi
 if [ ! -f "$CACHE_PATH/$IMAGE_ARCHIVE" ]; then
     print_info "Downloading $IMAGE_ARCHIVE from $IMAGE_URL..."
-    curl -s "$IMAGE_URL$IMAGE_ARCHIVE" > "$CACHE_PATH/$IMAGE_ARCHIVE"
+    curl -s "$IMAGE_URL/$IMAGE_ARCHIVE" > "$CACHE_PATH/$IMAGE_ARCHIVE"
 fi
 if [ ! -f "$CACHE_PATH/$SOURCES_ARCHIVE" ]; then
     print_info "Downloading $SOURCES_ARCHIVE from $SOURCES_URL..."
-    curl -s "$SOURCES_URL$SOURCES_ARCHIVE" > "$CACHE_PATH/$SOURCES_ARCHIVE"
+    curl -s "$SOURCES_URL/$SOURCES_ARCHIVE" > "$CACHE_PATH/$SOURCES_ARCHIVE"
 fi
 # ==============================================================================
 
@@ -155,6 +155,7 @@ fi
 print_info "Extracting image..."
 tar xf "$CACHE_PATH/$IMAGE_ARCHIVE" -C "$BUILD_PATH"
 print_info "Extracting sources file..."
+ls "$CACHE_PATH/$SOURCES_ARCHIVE"
 tar xf "$CACHE_PATH/$SOURCES_ARCHIVE" -C "$BUILD_PATH"
 
 print_info "Preparing image for CI..."
