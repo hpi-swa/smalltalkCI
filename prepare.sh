3b6c6a3069dea4386a8eae380a60ea30a2bb9f00
Fabio Niephaus <code@fniephaus.com>
2015-08-27 17:23:14 +0100

    Debug

--- prepare.sh
+++ prepare.sh
@@ -135,6 +135,7 @@ mkdir "$BUILD_PATH"
 if [ ! -f "$CACHE_PATH/$COG_VM_FILE" ]; then
     print_info "Downloading virtual machine..."
     curl -s "$VM_DOWNLOAD/$COG_VM_FILE" > "$CACHE_PATH/$COG_VM_FILE"
+    print_info "$VM_DOWNLOAD/$COG_VM_FILE"
 fi
 if [ ! -f "$COG_VM_PATH" ]; then
     print_info "Extracting virtual machine..."
@@ -155,7 +156,7 @@ fi
 print_info "Extracting image..."
 tar xf "$CACHE_PATH/$IMAGE_ARCHIVE" -C "$BUILD_PATH"
 print_info "Extracting sources file..."
-ls "$CACHE_PATH/$SOURCES_ARCHIVE"
+print_info "$CACHE_PATH/$SOURCES_ARCHIVE"
 tar xf "$CACHE_PATH/$SOURCES_ARCHIVE" -C "$BUILD_PATH"
 
 print_info "Preparing image for CI..."
