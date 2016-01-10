442645a14787a6721a733afc8773fa33302ee96d
Fabio Niephaus <code@fniephaus.com>
2016-01-10 19:07:00 +0000

    Debug bash test more

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -13,8 +13,6 @@ test_determine_project_home() {
 
   determine_project_home "../"
   echo "Check 1.1"
-  assertNotNull "${config_project_home}"
-  echo "Check 1.2"
   assertEquals "/" "${config_project_home:0:1}"
   echo "Check 2"
 
