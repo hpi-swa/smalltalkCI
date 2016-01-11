878ca15a94d083d93f8f9c46a954034f070e91a6
Fabio Niephaus <code@fniephaus.com>
2016-01-11 21:05:53 +0000

    Update GemStone run.sh for #20

--- gemstone/run.sh
+++ gemstone/run.sh
@@ -5,33 +5,15 @@ set -e
 readonly GS_DEVKIT_DOWNLOAD="https://github.com/GsDevKit/GsDevKit_home.git"
 
 ################################################################################
-# Check options and set defaults if unavailable.
-# Locals:
-#   config_baseline_group
-#   config_directory
-#   config_tests
-################################################################################
-gemstone::check_options() {
-  is_empty "${config_baseline_group}" && config_baseline_group="default"
-  is_empty "${config_directory}" && config_directory=""
-  is_empty "${config_devkit_branch}" && config_devkit_branch="dev"
-  is_empty "${config_gemstone_version}" && config_gemstone_version=`echo ${config_smalltalk} | cut -f2 -d-`
-  is_empty "${config_stone_name}" && config_stone_name="travis"
-  return 0
-}
-
-################################################################################
 # Clone the GsDevKit_home project. 
-# Arguments:
-#   devkit_branch
 ################################################################################
 gemstone::prepare_gsdevkit_home() {
-  local devkit_branch=$1
+  local devkit_branch="dev"
 
-  git clone ${GS_DEVKIT_DOWNLOAD}
-  cd GsDevKit_home
-  git checkout ${devkit_branch}
-  export GS_HOME=`pwd`
+  git clone "${GS_DEVKIT_DOWNLOAD}"
+  cd "GsDevKit_home"
+  git checkout "${devkit_branch}"
+  export GS_HOME="$(pwd)"
   return 0
 }
 
@@ -39,11 +21,13 @@ gemstone::prepare_gsdevkit_home() {
 # Create a GemStone stone. 
 # Arguments:
 #   stone_name
-#   gemstone_version
+#   config_smalltalk
 ################################################################################
 gemstone::prepare_stone() {
   local stone_name=$1
-  local gemstone_version=$2
+  local gemstone_version
+
+  gemstone_version="$(echo $2 | cut -f2 -d-)"
 
   # uncomment the following when os prereqs handled in smalltalk.rb - https://github.com/hpi-swa/smalltalkCI/issues/28
   # touch $GS_HOME/bin/.gsdevkitSysSetup  # Operating system setup already performed
@@ -54,46 +38,27 @@ gemstone::prepare_stone() {
 }
 
 ################################################################################
-# Load project into GemStone stone.
+# Load project into GemStone stone and run tests.
 # Arguments:
 #   stone_name
 # Locals:
-#   config_baseline
-#   config_baseline_group
-#   config_directory
 #   config_project_home
+# Globals:
+#   SMALLTALK_CI_HOME
 # Returns:
 #   Status code of project loading
 ################################################################################
-gemstone::load_project() {
+gemstone::load_and_test_project() {
   local stone_name=$1
 
   print_info "Loading project..."
 
   $GS_HOME/bin/devKitCommandLine serverDoIt ${stone_name} << EOF
-    Metacello new baseline: '${config_baseline}';
-      repository: 'filetree://${config_project_home}/${config_directory}';
-      load: '${config_baseline_group}'.
+    (BinaryOrTextFile openReadOnServer: '${SMALLTALK_CI_HOME}/lib/SmalltalkCI-Core.st') 
+      fileIn;
+      close.
+    SCISpec automatedTestOf: '${project_home}/smalltalk.ston'
     System commitTransaction.
-EOF
-  return $?
-}
-
-################################################################################
-# Run tests in GemStone stone.
-# Arguments:
-#   stone_name
-#   The name of project to test
-# Returns:
-#   Status code of build
-################################################################################
-gemstone::run_tests() {
-  local stone_name=$1
-  local project_name=$2
-
-  print_info "Run tests..."
-  $GS_HOME/bin/devKitCommandLine todeIt ${stone_name} << EOF
-    test --batch project ${project_name}
 EOF
   return $?
 }
@@ -104,6 +69,7 @@ EOF
 #   Status code of build
 ################################################################################
 run_build() {
+  local stone_name="travis"
   local exit_status=0
 
   # not needed when https://github.com/hpi-swa/smalltalkCI/issues/28 fixed
@@ -113,17 +79,9 @@ run_build() {
     sudo scutil --set HostName travis.dev
   fi
 
-  gemstone::check_options
-  gemstone::prepare_gsdevkit_home "${config_devkit_branch}"
-  gemstone::prepare_stone "${config_stone_name}" "${config_gemstone_version}"
-  gemstone::load_project "${config_stone_name}" || exit_status=$?
-
-  if [[ ! ${exit_status} -eq 0 ]]; then
-    print_error "Project could not be loaded."
-    return "${exit_status}"
-  fi
-
-  gemstone::run_tests "${config_stone_name}" "${config_baseline}" || exit_status=$?
+  gemstone::prepare_gsdevkit_home
+  gemstone::prepare_stone "${stone_name}" "${config_smalltalk}"
+  gemstone::load_and_test_project "${stone_name}" || exit_status=$?
 
   return "${exit_status}"
 }
