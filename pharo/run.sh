ee6ec8fad8518bb0ca664da3c530d7d8ae8b9ade
Esteban Lorenzano <estebanlm@gmail.com>
2016-01-26 15:02:12 +0100

    use asFileReference fileIn to... fileIn the code :)

--- pharo/run.sh
+++ pharo/run.sh
@@ -176,13 +176,8 @@ pharo::load_and_test_project() {
   travis_fold start load_and_test "Loading and testing project..."
     timer_start
 
-    "${SMALLTALK_CI_VM}" "${SMALLTALK_CI_IMAGE}" eval --save "
-      | stream |
-      stream := '${SMALLTALK_CI_HOME}/lib/SmalltalkCI-Core.st'.
-      stream := StandardFileStream oldFileNamed: stream.
-      stream := MultiByteFileStream newFrom: stream.
-      stream fileIn.
-      stream close.
+    "${SMALLTALK_CI_VM}" "${SMALLTALK_CI_IMAGE}" eval "
+      '${SMALLTALK_CI_HOME}/lib/SmalltalkCI-Core.st' asFileReference fileIn.
       (Smalltalk at: #SmalltalkCI) runCIFor: '${project_home}/${SMALLTALK_CI_DEFAULT_CONFIG}'
     " || status=$?
 
