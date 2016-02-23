c7008320f01bdc25d8df7220dd304166f222c9e6
Fabio Niephaus <code@fniephaus.com>
2016-02-23 23:44:41 +0000

    Remove $SMALLTALK_CI_BUILD_ID and move Travis builds to `_builds`

--- tests/fail_test.sh
+++ tests/fail_test.sh
@@ -9,13 +9,13 @@ if ! is_travis_build; then
   print_error_and_exit "This test needs to run on Travis."
 fi
 
-if is_empty "${SMALLTALK_CI_HOME}" || is_empty "${SMALLTALK_CI_BUILD_BASE}"; then
-  print_error_and_exit "SMALLTALK_CI_HOME or SMALLTALK_CI_BUILD_BASE not set."
+if is_empty "${SMALLTALK_CI_HOME}" || is_empty "${SMALLTALK_CI_BUILD}"; then
+  print_error_and_exit "SMALLTALK_CI_HOME or SMALLTALK_CI_BUILD not set."
 fi
 
-if is_dir "${SMALLTALK_CI_BUILD_BASE}/travis"; then
-  print_info "Removing old build folder at ${SMALLTALK_CI_BUILD_BASE}/travis"
-  rm -rf "${SMALLTALK_CI_BUILD_BASE}/travis"
+if is_dir "${SMALLTALK_CI_BUILD}"; then
+  print_info "Removing old build folder at ${SMALLTALK_CI_BUILD}"
+  rm -rf "${SMALLTALK_CI_BUILD}"
 fi
 
 print_info "Starting second pass to check that smalltalkCI can fail..."
