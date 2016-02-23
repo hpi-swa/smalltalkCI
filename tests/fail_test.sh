42297ed83e8d69808efc1ec1cdefc5874f6fe73e
Fabio Niephaus <code@fniephaus.com>
2016-02-23 23:31:43 +0000

    Disable caching and improve fail test #65

--- tests/fail_test.sh
+++ tests/fail_test.sh
@@ -13,9 +13,9 @@ if is_empty "${SMALLTALK_CI_HOME}" || is_empty "${SMALLTALK_CI_BUILD_BASE}"; the
   print_error_and_exit "SMALLTALK_CI_HOME or SMALLTALK_CI_BUILD_BASE not set."
 fi
 
-if is_dir "${SMALLTALK_CI_BUILD_BASE}"; then
-  print_info "Removing old build folder at ${SMALLTALK_CI_BUILD_BASE}"
-  rm -rf "${SMALLTALK_CI_BUILD_BASE}"
+if is_dir "${SMALLTALK_CI_BUILD_BASE}/travis"; then
+  print_info "Removing old build folder at ${SMALLTALK_CI_BUILD_BASE}/travis"
+  rm -rf "${SMALLTALK_CI_BUILD_BASE}/travis"
 fi
 
 print_info "Starting second pass to check that smalltalkCI can fail..."
