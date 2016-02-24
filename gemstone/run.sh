22a4845cd07d67f748c6ecb93f88275398106a84
Fabio Niephaus <code@fniephaus.com>
2016-02-24 22:34:19 +0000

    Minor cleanups

--- gemstone/run.sh
+++ gemstone/run.sh
@@ -2,7 +2,9 @@
 
 set -e
 
+readonly GS_STONE_NAME="travis"
 readonly GS_DEVKIT_DOWNLOAD="https://github.com/GsDevKit/GsDevKit_home.git"
+export GS_HOME="${SMALLTALK_CI_BUILD}/GsDevKit_home"
 
 ################################################################################
 # Clone the GsDevKit_home project.
@@ -13,11 +15,10 @@ gemstone::prepare_gsdevkit_home() {
   travis_fold start clone_gsdevkit "Cloning GsDevKit..."
     timer_start
 
-    pushd $SMALLTALK_CI_BUILD || exit 1
+    pushd "${SMALLTALK_CI_BUILD}" || exit 1
       git clone "${GS_DEVKIT_DOWNLOAD}" || exit 1
-      cd "GsDevKit_home" || exit 1
+      cd "${GS_HOME}" || exit 1
       git checkout "${devkit_branch}" || exit 1
-      export GS_HOME="$(pwd)"
 
       # pre-clone /sys/local, so that travis can skip backups
       $GS_HOME/bin/private/clone_sys_local || exit 1
@@ -37,15 +38,11 @@ gemstone::prepare_gsdevkit_home() {
 ################################################################################
 # Create a GemStone stone.
 # Arguments:
-#   stone_name
 #   config_smalltalk
 ################################################################################
 gemstone::prepare_stone() {
-  local stone_name=$1
   local gemstone_version
-
-  gemstone_version="$(echo $2 | cut -f2 -d-)"
-
+  gemstone_version="$(echo $1 | cut -f2 -d-)"
 
   travis_fold start install_server "Installing server..."
     timer_start
@@ -58,7 +55,7 @@ gemstone::prepare_stone() {
   travis_fold start create_stone "Creating stone..."
     timer_start
 
-    $GS_HOME/bin/createStone $stone_name $gemstone_version || print_error_and_exit "createStone failed."
+    $GS_HOME/bin/createStone "$GS_STONE_NAME" $gemstone_version || print_error_and_exit "createStone failed."
 
     timer_finish
   travis_fold end create_stone
@@ -67,8 +64,6 @@ gemstone::prepare_stone() {
 
 ################################################################################
 # Load project into GemStone stone and run tests.
-# Arguments:
-#   stone_name
 # Locals:
 #   config_project_home
 #   config_ston
@@ -78,13 +73,12 @@ gemstone::prepare_stone() {
 #   Status code of project loading
 ################################################################################
 gemstone::load_and_test_project() {
-  local stone_name=$1
   local status=0
 
   travis_fold start load_and_test "Loading and testing project..."
     timer_start
 
-    $GS_HOME/bin/devKitCommandLine serverDoIt ${stone_name} << EOF || status=$?
+    $GS_HOME/bin/devKitCommandLine serverDoIt "${GS_STONE_NAME}" << EOF || status=$?
       Metacello new
         baseline: 'SmalltalkCI';
 	repository: 'filetree://${SMALLTALK_CI_HOME}/repository';
@@ -93,7 +87,7 @@ gemstone::load_and_test_project() {
       System commitTransaction.
 EOF
 
-    $GS_HOME/bin/stopStone -b $stone_name || print_error_and_exit "stopStone failed."
+    $GS_HOME/bin/stopStone -b "${GS_STONE_NAME}" || print_error_and_exit "stopStone failed."
 
     timer_finish
   travis_fold end load_and_test
@@ -107,12 +101,11 @@ EOF
 #   Status code of build
 ################################################################################
 run_build() {
-  local stone_name="travis"
   local exit_status=0
 
   gemstone::prepare_gsdevkit_home
-  gemstone::prepare_stone "${stone_name}" "${config_smalltalk}"
-  gemstone::load_and_test_project "${stone_name}" || exit_status=$?
+  gemstone::prepare_stone "${config_smalltalk}"
+  gemstone::load_and_test_project || exit_status=$?
 
   return "${exit_status}"
 }
