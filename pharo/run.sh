eba9774e216c3cb62da3c40c0a5251d85abeefdf
Quentin Plessis <quentin@sorabito.com>
2017-10-04 20:44:04 +0900

    Move specified base image and changes to target folder.

--- pharo/run.sh
+++ pharo/run.sh
@@ -190,7 +190,8 @@ pharo::prepare_image() {
 
   if [ -n "${SMALLTALK_CI_BASE_IMAGE_FOLDER+set}" ]; then
     # Use given image as base image
-    cp -r "${SMALLTALK_CI_BASE_IMAGE_FOLDER}/*" "${target}/"
+    cp -r "${SMALLTALK_CI_BASE_IMAGE_FOLDER}/"*.image "${target}/"
+    cp -r "${SMALLTALK_CI_BASE_IMAGE_FOLDER}/"*.changes "${target}/"
   else
     # Download new base image
     local pharo_image_url="$(pharo::get_image_url "${smalltalk_name}")"
