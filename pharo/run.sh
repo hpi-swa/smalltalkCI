323dd9e221592fe82baf5a7ecabd4fec3104a4c0
Gabriel Omar Cotelli <g.cotelli@gmail.com>
2019-01-22 14:00:02 -0300

    Move missing stable definition

--- pharo/run.sh
+++ pharo/run.sh
@@ -126,10 +126,10 @@ pharo::get_vm_url() {
     "Pharo32-8.0")
       echo "get.pharo.org/vm80"
       ;;
-    "Pharo32-stable"|"Pharo32-7.0"|"Pharo-7.0")
+    "Pharo-stable"|"Pharo32-stable"|"Pharo32-7.0"|"Pharo-7.0")
       echo "get.pharo.org/vm70"
       ;;
-    "Pharo32-6.1"|"Moose32-trunk"|"Moose32-6.1"|"Pharo-stable"|"Pharo-6.1"|"Moose-trunk"|"Moose-6.1")
+    "Pharo32-6.1"|"Moose32-trunk"|"Moose32-6.1"|"Pharo-6.1"|"Moose-trunk"|"Moose-6.1")
       echo "get.pharo.org/vm61"
       ;;
     "Pharo32-6.0"|"Pharo-6.0")
