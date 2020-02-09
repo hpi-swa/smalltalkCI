bb6066c74361c1d7b201ddd387ccfc66e8389492
Fabio Niephaus <code@fniephaus.com>
2020-02-10 00:32:27 +0100

    "MSYS_NT"* fixes

--- squeak/run.sh
+++ squeak/run.sh
@@ -167,7 +167,7 @@ squeak::get_vm_details() {
       vm_file_ext="dmg"
       vm_path="${config_vm_dir}/Squeak.app/Contents/MacOS/Squeak"
       ;;
-    "CYGWIN_NT-"*)
+    "MSYS_NT"*|"CYGWIN_NT-"*)
       if is_64bit; then
         vm_arch="win64x64"
       else
@@ -244,7 +244,7 @@ squeak::determine_vm_flags() {
       "Linux")
         vm_flags="-nosound -vm-display-null"
         ;;
-      "Darwin"|"CYGWIN_NT-"*)
+      "Darwin"|"MSYS_NT"*|"CYGWIN_NT-"*)
         vm_flags="-headless"
         ;;
     esac
