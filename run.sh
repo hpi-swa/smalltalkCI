d38be5a4b38ab824ff706c4b54d3e9656ede96e2
Max Leske <maxleske@gmail.com>
2020-02-29 18:48:45 +0100

    When smalltalk_config is a ruby array string, extract the first value

--- run.sh
+++ run.sh
@@ -129,7 +129,9 @@ ensure_ston_config_exists() {
 
   if is_travis_build; then
     if is_not_empty "${TRAVIS_SMALLTALK_CONFIG:-}"; then
-      config_ston="${TRAVIS_BUILD_DIR}/${TRAVIS_SMALLTALK_CONFIG}"
+      # if the variable is a list (ruby array like ["a", "b", "c"]) extract the first value
+      first_config=echo ${TRAVIS_SMALLTALK_CONFIG//[\[\]]} | awk -F',' '{print $1}'
+      config_ston="${TRAVIS_BUILD_DIR}/${first_config}"
     else
       locate_ston_config
     fi
