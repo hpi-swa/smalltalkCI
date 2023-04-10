b672ab610e0336e0f530ddf15f97f4d8cd9cf511
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2023-04-09 21:28:01 -0700

    address 'line 535: config_tracking: unbound variable' error that popped up

--- helpers.sh
+++ helpers.sh
@@ -532,8 +532,10 @@ report_build_metrics() {
   local duration=$(($(timer_nanoseconds)-$smalltalk_ci_start_time))
   duration=$(echo "${duration}" | awk '{printf "%.3f\n", $1/1000000000}')
 
-  if [[ "${config_tracking}" != "true" ]]; then
-    return 0
+  if [ "${config_tracking+set}" ] then
+  	if [[ "${config_tracking}" != "true" ]]; then
+    	return 0
+		fi
   fi
 
   if is_travis_build; then
